d7fb1a12be84bd1bfb1d945d0efba8128b10e784
fernandosfn06 <143672112+fernandosfn06@users.noreply.github.com>
2026-02-21 12:00:42 -0600

    Tarea: Inicio de programacion en javascript

---
+++ tarea2/nombres.js
@@ -0,0 +1,11 @@
+const nombre = prompt( "Escribe tu nombre completo: ");
+console.log ("Nombre: " + nombre);
+
+var edad= prompt ("Escribe tu edad: ");
+console.log("Edad: " +edad);
+
+const carrera = prompt("Escribe tu carrera: ");
+console.log("Carrera: " +carrera);
+
+const correo = prompt("Escribe tu correo electronico: ");
+console.log("Correo electronico: " +correo);
